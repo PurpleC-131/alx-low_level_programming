c6eda3a4c0ce61a0b6311deda077bbfb9206c572
PurpleC-131 <hartharris63@gmail.com>
2022-10-14 00:48:42 -0700

    task 4&6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,12 @@
 
 /**
  * main - prints "Programming is like building a multilingual puzzle
- * @void:Function argument
- * Return Always 0 (Success)
+ * @void: Function argument
+ * Return: 0 if the code runs successfuly. 1 otherwise
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
